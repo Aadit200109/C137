b7774d48c294e30c6c2ac8bba95e28339bffcd08
Aadit200109 <89086990+Aadit200109@users.noreply.github.com>
2022-05-17 20:40:27 -0400

    Update main.js

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ function draw() {
             document.getElementById("number_of_objects").innerHTML = "Number of objects detected are : "+ objects.length;
 
             fill("#FF0000");
-            percent = flor(objects[i].confidence * 100);
+            percent = floor(objects[i].confidence * 100);
             Text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
             noFill();
 
